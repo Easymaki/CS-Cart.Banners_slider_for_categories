03d5de142f9154adbf1ea0b25dda0dc15e24aeb8
Nikita <85439842+Easymaki@users.noreply.github.com>
2022-06-09 14:19:26 +0400

    Update func.php

--- category_slider/app/addons/category_slider/func.php
+++ category_slider/app/addons/category_slider/func.php
@@ -4,7 +4,7 @@ if (!defined('BOOTSTRAP')) {
     die('Access denied');
 }
 
-function fn_category_slider_get_category_data_post(&$category_id, $field_list, $get_main_pair, $skip_company_condition, $lang_code, &$category_data)
+function fn_category_slider_get_category_data_post($category_id, $field_list, $get_main_pair, $skip_company_condition, $lang_code, &$category_data)
 {
     $category_data['main_pair'] = fn_get_image_pairs($category_id, 'category', 'M', true, true, $lang_code);
     $category_data['image_pairs'] = fn_get_image_pairs($category_id, 'category', 'A', true, true, $lang_code);
@@ -16,7 +16,7 @@ function fn_category_slider_get_category_data_post(&$category_id, $field_list, $
     }
 }
 
-function fn_category_slider_update_category_post(&$category_data, &$category_id, $lang_code, $create)
+function fn_category_slider_update_category_post($category_data, $category_id, $lang_code, $create)
 {
     fn_attach_image_pairs('category_main', 'category', $category_id, $lang_code);
     fn_attach_image_pairs('category_additional', 'category', $category_id, $lang_code);
